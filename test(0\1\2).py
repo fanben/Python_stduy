a9bdcc264fe5f48a7e361e45ff450bf7a5b2166d
Elan_Fan <457954437@qq.com>
2018-12-25 17:50:57 +0800

    增加5次循环

--- test(0\1\2).py
+++ test(0\1\2).py
@@ -2,17 +2,22 @@
 # coding=UTF-8
 
 import random
-playerInput = input("请输入（0剪刀 1石头 2布）：")
 
-player = int(playerInput)
+i = 0
+while i < 5:
+	playerInput = input("请输入（0剪刀 1石头 2布）：")
 
-mac = random.randint(0,2)
+	player = int(playerInput)
 
-if (player == 0 and mac == 2) or (player == 1 and mac == 0) or (player == 2 and mac == 2):
-	print ("您获胜了！")
+	mac = random.randint(0,2)
 
-elif player == mac:
-	print("平局，要不要再来一局？")
+	if (player == 0 and mac == 2) or (player == 1 and mac == 0) or (player == 2 and mac == 2):
+		print ("您获胜了！")
 
-else:
-	print("您输了！")
+	elif player == mac:
+		print("平局，要不要再来一局？")
+
+	else:
+		print("您输了！")
+
+	i +=1
