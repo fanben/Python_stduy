c706ebb2ef3765a9078e56469ef025972a2cd76a
Elan_Fan <457954437@qq.com>
2019-03-22 19:32:04 +0800

    复制文件并重命名保存

--- copy.py
+++ copy.py
@@ -4,7 +4,10 @@ old_file = open(old_file_name,'r')
 
 content = old_file.read()
 
-new_file_name = "[复件]" + old_file_name
+position = old_file_name.rfind(".")
+new_file_name = old_file_name[:position] + "[复件]" + old_file_name[position:]
+
+# new_file_name = "[复件]" + old_file_name
 
 new_file = open(new_file_name,'w')
 
