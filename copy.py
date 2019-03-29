6c9ae17535e97770ddb2b3d1478e65a65c1e69bb
Elan_Fan <457954437@qq.com>
2019-03-29 11:10:32 +0800

    目前修改可复制数据比较大文件

--- copy.py
+++ copy.py
@@ -1,8 +1,6 @@
-old_file_name = input()
+old_file_name = input("请输入要复制的文件名:")
 
 old_file = open(old_file_name,'r')
-
-content = old_file.read()
 
 position = old_file_name.rfind(".")
 new_file_name = old_file_name[:position] + "[复件]" + old_file_name[position:]
@@ -11,7 +9,11 @@ new_file_name = old_file_name[:position] + "[复件]" + old_file_name[position:]
 
 new_file = open(new_file_name,'w')
 
-new_file.write(content)
+while True:
+	content = old_file.read(1024)
+	if len(content) == 0:
+		break
+	new_file.write(content)
 
 old_file.close()
 
